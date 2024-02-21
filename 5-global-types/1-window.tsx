863967a726de84dafb9017fad8cbcf6b2a02cef5
Eitler, Dominik <dominik.eitler@tuwien.ac.at>
2024-02-21 10:56:41 +0100

    minor change

--- 5-global-types/1-window.tsx
+++ 5-global-types/1-window.tsx
@@ -2,15 +2,14 @@
  * Window
  */
 
+// interface Window {
+//   global_customer_limits: object;
+//   pspdfkit_config: { license_key: string };
+//   user_id: number;
+// }
+
 const exists = window.location;
 const doesntExist = window.global_customer_limits;
 
 // --> window.d.ts
 // all types declared there are global
-// not merged yet
-
-interface Window {
-  global_customer_limits: object;
-  pspdfkit_config: { license_key: string };
-  user_id: number;
-}
